bae85f86f99f5358351d697323ec2f25de4727ca
ihorkoi <koleev.igor@gmail.com>
2023-09-04 13:14:43 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,11 +1,10 @@
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-import { List } from 'components/ImageGallery/ImageGallery.styled';
 
 export const ImageGallery = ({ images }) => {
   return (
     <ul className="imageGallery">
-      {images.map(({ id, webformatURL, largeImageURL }) => {
-        return <ImageGalleryItem image={{ id, webformatURL, largeImageURL }} />;
+      {images.map(image => {
+        return <ImageGalleryItem image={image} />;
       })}
     </ul>
   );
